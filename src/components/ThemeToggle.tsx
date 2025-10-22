5bda3068f8f7ad533ad0adcdc0616075ffd29ed8
Joao <232938876+joaokhalaf@users.noreply.github.com>
2025-10-22 17:32:16 -0300

    fix: mounted button

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -3,12 +3,22 @@
 import { Moon, Sun } from 'lucide-react'
 import { useTheme } from 'next-themes'
 import { Button } from './ui/button'
+import { useEffect, useState } from 'react';
 
 export function ThemeToggle() {
+  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme()
+
+  useEffect(() => {
+    setMounted(true)
+  }, []);
 
   const toggleTheme = () => {
     setTheme(theme === 'dark' ? 'light' : 'dark')
+  }
+
+  if(!mounted) {
+    return <Button variant="outline" size="icon" disabled={true} />
   }
 
   return (
