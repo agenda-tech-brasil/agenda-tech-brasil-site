75c95fdd3092161bed9e70c756a909a00551e9d2
fabiobrasileiroo <fabio.h591@gmail.com>
2026-01-22 14:12:50 -0400

    fix: update fetch URL for event data source

--- src/utils/fetchEvents.ts
+++ src/utils/fetchEvents.ts
@@ -2,11 +2,12 @@ import { Evento } from '@/@types/events'
 
 export async function fetchEvents(): Promise<Evento[]> {
   const response = await fetch(
-    'https://raw.githubusercontent.com/Abacatinhos/agenda-tech-brasil/refs/heads/main/src/db/database.json',
+    'https://raw.githubusercontent.com/agenda-tech-brasil/agenda-tech-brasil/refs/heads/main/src/db/database.json',
   )
   if (!response.ok) {
     throw new Error('Failed to fetch events')
   }
   const data = await response.json()
+  console.log("🚀 ~ fetchEvents ~ data:", data)
   return data.eventos
 }
